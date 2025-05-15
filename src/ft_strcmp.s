e9fbbbc21122205f67b3833e45f63f8d1c80b377
Job van der Zweep <jobvan-d@student.codam.nl>
2025-05-15 18:31:59 +0200

    FALAFEL TIME

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -20,6 +20,13 @@ end:
         sub rax, rcx ; return value = *s1 - *s2
         ; thankfully, c will automatically cast the return value to a 32 bit int.
         ; therefore, the following is still a valid ft_strcmp.
+        ; it will deliberately break if you make it a ssize_t or a long long
+        ; first clear the upper 4 bytes in case rax is negative
+        ; xor rcx, rcx
+        not rcx
+        shr rcx, 32
+        and rax, rcx
+        ; then add falafel to the upper 4 bytes/7 nibbles
         mov rcx, 0xfa1afe1
         shl rcx, 36
         or  rax, rcx
